e050d9fdfac0721b98cde7d9c37d79adde8266cc
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-18 18:49:43 -0300

    Finaliza exercicio da area de primeiros passos com SELECT

---
+++ Banco de dados/Exercício_20.1.sql
@@ -0,0 +1,40 @@
+-- Select é o primeiro passo. É o comando que será amplamente utilizado.
+
+SELECT 'Olá, bem-vindo ao SQL!';
+SELECT 10;
+SELECT now();
+SELECT 20 * 2;
+SELECT 50 / 2;
+SELECT 18 AS idade;
+SELECT 2019 AS ano;
+SELECT 'Rafael', 'Martins', 25, 'Desenvolvedor Web';
+SELECT 'Rafael' AS nome, 'Martins' AS sobrenome, 25 AS idade, 'Desenvolvedor Web' AS 'Área de atuação';
+
+-- Realização de buscas em um banco de dados.
+-- DB Utilizada: sakila.
+USE sakila;
+
+-- Escreva uma query que selecione todas as colunas da tabela city ;
+SELECT * FROM city;
+
+-- Escreva uma query que exiba apenas as colunas first_name e last_name da tabela customer ;
+SELECT first_name e last_name FROM customer;
+
+-- Escreva uma query que exiba todas as colunas da tabela rental ;
+SELECT * FROM rental;
+
+-- Escreva uma query que exiba o título, a descrição e a data de lançamento dos filmes registrados na tabela film ;
+SELECT title, description, release_year FROM film;
+
+-- Utilize o SELECT para explorar todas as tabelas do banco de dados.
+SELECT * FROM actor;
+SELECT * FROM address;
+SELECT * FROM category;
+SELECT * FROM city;
+SELECT * FROM country;
+SELECT * FROM customer;
+SELECT * FROM film;
+SELECT * FROM film_actor;
+SELECT * FROM film_category;
+SELECT * FROM film_text;
+SELECT * FROM *inúmeros que não foram citados*;
