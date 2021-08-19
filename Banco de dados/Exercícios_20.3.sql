fab4658233af0754e2f60a167e64851d019cf4c2
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-19 17:36:31 -0300

    Pausa para a aula

--- Banco de dados/Exercícios_20.3.sql
+++ Banco de dados/Exercícios_20.3.sql
@@ -61,6 +61,36 @@ WHERE rating = 'NC-17'
 ORDER BY replacement_cost ASC;
 ----------------------------------------------------------------------------
 
--- Criar consultas mais dinâmicas e maleáveis com LIKE .
+-- Criar consultas mais dinâmicas e maleáveis com LIKE.
+-- O LIKE é usado para buscar por meio de uma sequência específica de caracteres
+-- % O sinal de percentual, que pode representar zero, um ou múltiplos caracteres
+-- O underlin, que representa um único caractere
+-- Resultado espefícico no final da string
+SELECT * FROM sakila.film
+WHERE title LIKE '%don';
+
+-- Resultado específico no início da string
+SELECT * FROM sakila.film
+WHERE title LIKE 'don%';
+
+-- Resultado que contenha qualquer coisa referente entre dois %
+SELECT * FROM sakila.film
+WHERE title LIKE '%plu%';
+
+-- Encontra qualquer resultado que inicia com "p" e finaliza com "r"
+SELECT * FROM sakila.film
+WHERE title LIKE 'p%r';
+
+-- Encontra qualquer resultado em que o segundo caractere da frase é "C"
+SELECT * FROM sakila.film
+WHERE title LIKE '_C%';
+
+-- Encontra qualquer resultado em que o título possui exatamente 8 caracteres
+SELECT * FROM sakila.film
+WHERE title LIKE '________';
+
+-- Encontra todas as palavras com no mínimo 3 caracteres e que iniciam com E
+SELECT * FROM sakila.film
+WHERE title LIKE 'E__%';
 -- Fazer consultas que englobam uma faixa de resultados com IN e BETWEEN .
 -- Encontrar e separar resultados que incluem datas.
