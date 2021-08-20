62c5f8552fde643ae2af9f9a90b078e03da028d2
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-20 19:03:53 -0300

    Inicia exercicios dia 20.4

---
+++ Banco de dados/Exercícios_20.4.sql
@@ -0,0 +1,4 @@
+-- Tabela vazia ? Vamos resolver isso ae! INSERT na prática
+-- Inserindo dados em uma coluna específica:
+INSERT INTO nome_da_tabela (coluna1, coluna2)
+VALUES ('valor_coluna1', 'valor_coluna2');
