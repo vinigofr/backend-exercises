554d310c1708682efa55d4919d8e319409648115
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-19 21:12:25 -0300

    Finaliza exercicio de conteudo 20.2

--- Banco de dados/Exercícios_20.2.sql
+++ Banco de dados/Exercícios_20.2.sql
@@ -156,3 +156,38 @@ SELECT * FROM sakila.address
 ORDER BY address(ASC || DESC), district(ASC || DESC);
 -- As columas podem ser ordenadas separadamente.
 
+-- Exercício de fixação 20.2
+-- Para os exercícios a seguir, vamos usar a tabela sakila.film
+USE sakila;
+--
+-- Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
+SELECT * from film;
+--
+-- Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
+SELECT title, release_year, rating from film;
+--
+-- Quantos filmes temos cadastrados?
+SELECT COUNT(*) FROM film;
+
+-- Para os exercícios a seguir, vamos usar a tabela sakila.actor
+-- Escreva uma query que exiba apenas os sobrenomes únicos cadastrados.
+SELECT DISTINCT last_name FROM actor;
+-- Quantos sobrenomes únicos temos na tabela?
+SELECT COUNT(DISTINCT last_name) FROM actor;
+--
+-- Ordene os valores na tabela em ordem crescente de sobrenomes e em ordem decrescente de nome.
+SELECT last_name, first_name FROM actor
+ORDER BY last_name ASC, first_name DESC;
+--
+-- Vá até a tabela language do sakila e crie uma pesquisa que mostre os 5 idiomas cadastrados,
+-- mas não mostre o idioma english.
+SELECT * FROM language
+WHERE name <> 'english';
+--
+-- Agora vamos tentar fazer o seguinte: Crie uma query para encontrar os 20 primeiros filmes,
+-- incluindo o título , o ano de lançamento , a duração , a classificação indicativa e o custo
+-- de substituição . Ordene os resultados pelos filmes com a maior duração e depois pelo menor
+-- custo de substituição.
+SELECT title, release_year, rental_duration, rating, replacement_cost FROM film
+ORDER BY rental_duration DESC, rating ASC
+LIMIT 20;
