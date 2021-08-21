0cd4aba22071914c8df332bab2e2c7fb43bda173
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-21 01:40:03 -0300

    Adiciona numeros aos exercicios

--- Banco de dados/Exercícios_20.3.sql
+++ Banco de dados/Exercícios_20.3.sql
@@ -199,13 +199,13 @@ SELECT MINUTE(payment_date) FROM sakila.payment; -- Minuto
 SELECT SECOND(payment_date) FROM sakila.payment; -- Segundo
 
 -- Atividade final de fixação
--- Quantos pagamentos temos com a data de retorno 2005-05-25 ? Há múltiplas maneiras possíveis de encontrar a resposta.
+-- 1 Quantos pagamentos temos com a data de retorno 2005-05-25 ? Há múltiplas maneiras possíveis de encontrar a resposta.
 SELECT payment_date FROM sakila.payment
 WHERE DATE(payment_date) = '2005/05/25';
--- Quantos pagamentos foram feitos entre 01/07/2005 e 22/08/2005 ?
+-- 2 Quantos pagamentos foram feitos entre 01/07/2005 e 22/08/2005 ?
 SELECT payment_date FROM sakila.payment
 WHERE DATE(payment_date) BETWEEN '2005/07/01' AND '2005/08/22';
--- Usando a tabela rental , extraia data, ano, mês, dia, hora, minuto e segundo dos registros com rental_id = 10330. Utilize a coluna rental_date para extrair as informações.
+-- 3 Usando a tabela rental , extraia data, ano, mês, dia, hora, minuto e segundo dos registros com rental_id = 10330. Utilize a coluna rental_date para extrair as informações.
 SELECT CONCAT(
 DATE(rental_date),
 ' ',
@@ -222,7 +222,9 @@ MINUTE(rental_date),
 SECOND(rental_date)
 ) AS 'Specific Infos' FROM sakila.rental
 WHERE rental_id = 10330;
--- Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
+-- 4 Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
 SELECT * FROM sakila.payment
 WHERE DATE(payment_date) = '2005/07/28'
 AND HOUR(payment_date) >= 22;
+
+----------------------------------------------------------------------------
