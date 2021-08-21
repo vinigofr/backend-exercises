1c3d539cfc2e9e97ffefa706345c4139cfe75bb8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-21 19:19:17 -0300

    Finaliza atividade de UPDATE

--- Banco de dados/Exercícios_20.4.sql
+++ Banco de dados/Exercícios_20.4.sql
@@ -94,3 +94,93 @@ SELECT * from store;
 
 INSERT INTO store (manager_staff_id, address_id)
 VALUES (3, 3);
+
+--------------------------
+-- O UPDATE permite alterar valores de uma tabela com base em alguma condição!
+-- exemplo:
+-- 1 Seleção da tabela a ser atualizada:
+UPDATE sakila.staff
+-- 2 Parâmetro para adição, selecionando a coluna
+SET first_name = 'Rannveig'
+-- 3 Parâmetro condincional, selecionando coluna
+WHERE first_name = 'Ravein';
+-- importantíssimo aplicar o WHERE para não alterar a tabela inteira!
+
+-- Uma curiosidade sobre o UPDATE e o DELETE no MySQL Server é que, por padrão, 
+-- existe uma configuração chamada safe updates mode que só vai te permitir executá-los 
+-- caso eles incluam quais IDs devem ser modificados. Então, caso você tente fazer 
+-- a query abaixo, ela não funcionaria por não incluir o ID.
+
+-- Para evitar essa restrição, rode o seguinte comando em uma janela de query dentro 
+-- do MySQL Workbench sempre que abri-lo para desabilitar essa funcionalidade, antes de 
+-- executar seus comandos de UPDATE ou DELETE :
+SET SQL_SAFE_UPDATES = 0;
+
+-- Tá, mas e pra atualizar mais de uma coluna?
+UPDATE sakila.staff
+SET first_name = 'Rannveig', last_name = 'Jordan'
+WHERE staff_id = 4;
+
+-- Pode-se também fazer um update em massa, isso evita gasto desnecessário de dados e não
+-- sobrecarrega o servidor
+-- Opção 1 - Incluindo a lista de condições fixas
+UPDATE sakila.actor
+SET first_name = 'JOE'
+WHERE actor_id IN (1,2,3);
+
+-- Opção 2 - Especificando como cada entrada será alterada individualmente
+UPDATE sakila.actor
+SET first_name = (
+CASE actor_id 
+    WHEN 1 THEN 'JOE' -- se actor_id = 1, alterar first_name para 'JOE'
+    WHEN 2 THEN 'DAVIS' -- se actor_id = 2, alterar first_name para 'DAVIS'
+    WHEN 3 THEN 'CAROLINE' -- se actor_id = 3, alterar first_name para 'CAROLINE'
+      ELSE first_name -- em todos os outros casos, mantém-se o first_name
+END);
+
+-- Fazendo UPDATE de forma sequencial, pera, pera.... Mas o que isso significa?
+-- Se o comando ORDER BY for usado juntamente com o UPDATE , os resultados serão alterados na ordem em que forem encontrados.
+-- Se o comando LIMIT for usado em conjunto com o UPDATE , um limite será imposto na quantidade de resultados que podem ser alterados.
+-- Caso contrário, todos os resultados que satisfizerem a condição serão atualizados.
+-- Exemplo:
+UPDATE nome_da_tabela
+SET coluna1 = valor1, coluna2 = valor2
+[WHERE condições]
+[ORDER BY expressao [ ASC | DESC ]]
+[LIMIT quantidade_resultados];
+
+-- Exemplo prático:
+UPDATE sakila.staff
+SET password = 'FavorResetarSuaSenha123'
+WHERE active = 1
+ORDER BY last_update
+LIMIT 2;
+
+--------------------------
+
+-- Atividade de UPDATE
+-- 1. Atualize o primeiro nome de todas as pessoas da tabela sakila.actor que possuem o primeiro nome "JULIA" para "JULES".
+UPDATE sakila.actor
+SET first_name = 'JULES'
+WHERE first_name = 'JULIA';
+
+-- 2. Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".
+UPDATE sakila.category
+SET name = 'Science Fiction'
+WHERE name = 'Sci-Fi';
+-- Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a
+--classificações "G" , "PG" ou "PG-13" e um custo de substituição maior que $20.
+UPDATE sakila.film
+SET replacement_cost = 25
+WHERE `length` > 100 AND rating IN ('PG', 'PG-13', 'G');
+
+-- Foi determinado pelo setor financeiro que haverá um reajuste em todos os 
+-- preços dos filmes, com base em sua duração. Para todos os filmes com duração 
+-- entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes
+-- com duração acima de 100 passará a ser de $20,00.
+UPDATE sakila.film
+SET rental_rate = (
+	CASE WHEN LENGTH <= 100 THEN 10
+    WHEN LENGTH > 100 THEN 20
+    ELSE rental_rate
+    END);
