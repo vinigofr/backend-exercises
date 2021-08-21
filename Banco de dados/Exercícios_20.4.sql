e75e99cb07fac6ae53e77b265ebd5a687e9a5336
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-21 12:47:25 -0300

    Finaliza exercicios de INSERT

--- Banco de dados/Exercícios_20.4.sql
+++ Banco de dados/Exercícios_20.4.sql
@@ -46,3 +46,51 @@ VALUES('Marcelo','Santos');
 -- Ta script acima tem como primeira coluna, actor_id, porém, o auto_increment está ativo.
 -- Tal coluna pode ser omitida no INSERT.
 
+--------------------------
+
+-- Inserindo dados de outra tabela com o INSERT SELECT
+-- É possível inserir dados a partir de outra tabela usando INSERT INTO SELECT
+INSERT INTO tabelaA (coluna1, coluna2)
+    SELECT tabelaB.coluna1, tabelaB.coluna2
+    FROM tabelaB
+    WHERE tabelaB.nome_da_coluna <> 'algumValor'
+    ORDER BY tabelaB.coluna_de_ordenacao;
+
+--É possível usar também SELECT * FROM e copiar todos os dados de 
+-- todas as colunas de uma tabela para outra, mas nessa situação a 
+-- tabelaA e a tabelaB precisam obrigatoriamente possuir a mesma quantidade
+-- de colunas, e os tipos de dados das colunas correspondentes devem ser iguais.
+
+-- Outro exemplo;
+INSERT INTO sakila.actor (first_name, last_name)
+  SELECT first_name, last_name FROM sakila.staff;
+
+-- Atividade de fixação
+--1 Insira um novo funcionário na tabela sakila.staff.
+INSERT INTO sakila.staff 
+(first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES 
+('Vinicius', 'Gouveia', 2, 'hahaha.com.br', 1, 1, 'vinigofr', 'password');
+--2 Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
+INSERT INTO sakila.staff 
+(first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES 
+('Augusto', 'Almeida', 2, 'hahaha.com.br', 1, 1, 'vinigofr', 'password'),
+('Junior', 'Mizerable', 2, 'hahaha.com.br', 1, 1, 'vinigofr', 'password');
+--3 Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer
+-- e cadastre essas pessoas como atores na tabela sakila.actor.
+INSERT INTO sakila.actor (first_name, last_name)
+SELECT first_name, last_name FROM sakila.customer
+LIMIT 5;
+--4 Cadastre três categorias de uma vez só na tabela sakila.category.
+SELECT * FROM sakila.category;
+SELECT * FROM sakila.customer;
+
+INSERT INTO sakila.category (name)
+VALUES ('CatA'), ('CatB'), ('CatB');
+--5 Cadastre uma nova loja na tabela sakila.store.
+USE sakila;
+SELECT * from store;
+
+INSERT INTO store (manager_staff_id, address_id)
+VALUES (3, 3);
