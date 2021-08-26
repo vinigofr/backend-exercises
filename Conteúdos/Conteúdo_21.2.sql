d2daf2b748b79a92bf1dc83bff35745e033a235e
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-26 13:07:26 -0300

    Adiciona atividade de fixaçao do 20.2

--- Conteúdos/Conteúdo_21.2.sql
+++ Conteúdos/Conteúdo_21.2.sql
@@ -259,7 +259,33 @@ FROM sakila.customer AS tabela_externa;
 -- Retorna dados de uma tabela T1 onde existe relacionamento com uma tabela T2. 
 -- O EXISTS espera uma SUBQUERY como parâmetro.
 
--- 1. Usando o EXISTS na tabela books_lent e books , exiba o id e título dos livros que ainda não foram emprestados.
--- 2. Usando o EXISTS na tabela books_lent e books , exiba o id e título dos livros estão atualmente emprestados e que contêm a palavra "lost" no título.
+-- 1. Usando o EXISTS na tabela books_lent e books, exiba o id e título dos livros que ainda não foram emprestados.
+SELECT Id, Title FROM hotel.Books
+WHERE EXISTS (
+SELECT book_rental FROM hotel.Books_Lent where returned = 1 AND book_rental = Id
+);
+
+-- 2. Usando o EXISTS na tabela books_lent e books , exiba o id e título dos livros estão atualmente
+-- emprestados e que contêm a palavra "lost" no título.
+SELECT b.Id, b.Title FROM hotel.Books AS b
+WHERE EXISTS (
+SELECT book_rental
+FROM hotel.Books_Lent
+WHERE returned = 0
+AND book_rental = b.Id
+AND b.Title
+LIKE '%lost%'
+);
 -- 3. Usando a tabela carsales e customers , exiba apenas o nome dos clientes que ainda não compraram um carro.
--- 4. Usando o comando EXISTS em conjunto com JOIN e as tabelas cars , customers e carsales , exiba o nome do cliente e o modelo do carro de todos os clientes que fizeram compras de carros.
+SELECT Name FROM hotel.Customers AS c
+WHERE NOT EXISTS (
+	SELECT CustomerID FROM hotel.CarSales WHERE CustomerID = c.CustomerId
+);
+-- 4. Usando o comando EXISTS em conjunto com JOIN e as tabelas cars, customers e carsales,
+-- exiba o nome do cliente e o modelo do carro de todos os clientes que fizeram compras de carros.
+SELECT cus.Name, car.Name FROM hotel.Customers AS cus
+INNER JOIN hotel.Cars AS car
+WHERE EXISTS (
+	select * from hotel.CarSales
+    WHERE CustomerID = cus.CustomerID AND CarID = car.ID
+);
